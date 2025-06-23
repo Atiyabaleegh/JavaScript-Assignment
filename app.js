36572fb96cea9963678108a1a689c79544a94caa
Atiya_Baleegh <attiya.baligh@gmail.com>
2025-06-23 09:38:37 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+alert ("Hello,Welcome to our website");
+alert ("Error! Please enter a valid password");
+alert ("Welcome to JS land \n  happy coding");
+console.log(("Hello, I can run JS through my web browser console"));
+// Make use of alerts in your new/existing HTML & CSS project
+"I attached JavaSCript Alert in existing html AND CSS project."
